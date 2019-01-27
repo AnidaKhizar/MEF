1e534aa2a627cb5db491b15cb1b1f11fbe4240d3
3407497 <anida.khizar@etu.upmc.com>
2019-01-27 19:56:03 +0100

    Added submarine mesh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ gmsh $1 -2                           # génération du maillage
 IFS='.' read -ra mesh <<< "$1"       # tronquer l'extension .geo du fichier d'entrée
 python2 write_output.py $mesh.msh    # résolution du problème
 
-mkdir bin
-cmake $PWD                           #importation de la triangulation
+mkdir -p bin                         
+cmake $PWD                           # génération du Makefile
 make
-./Importer
-paraview output.vtu                  #visualisation de la solution
+./Importer                           # importation de la triangulation en vtk
+paraview output.vtu                  # visualisation de la solution
